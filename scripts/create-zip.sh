bb2c15893b09ade4173473d09231eacd8117b166
InrcedibleHolg <programming18@verdunkelbar.de>
2022-01-27 09:04:10 +0100

    cleanup

--- scripts/create-zip.sh
+++ scripts/create-zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PACKNAME=IncediblePack
-GAMEVERSION=1.14
+GAMEVERSION=1.18
 DEST=$PACKNAME-$GAMEVERSION-$(date +'%y%m%d-%s').zip
 RSOURCEPACKS=$HOME/.minecraft/resourcepacks
 ( cd resourcepack && zip -r ../$DEST . )
