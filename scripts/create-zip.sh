8b387be39061a4604615fa29361e61c7eb0956f7
Holger Dunkel <holger.dunkel@gmail.com>
2022-02-05 11:56:36 +0100

    warped & crimson, noteblock

--- scripts/create-zip.sh
+++ scripts/create-zip.sh
@@ -3,7 +3,7 @@ PACKNAME=IncediblePack
 GAMEVERSION=1.18
 DEST=$PACKNAME-$GAMEVERSION-$(date +'%y%m%d-%s').zip
 RSOURCEPACKS=$HOME/.minecraft/resourcepacks
-( cd resourcepack && zip -r ../$DEST . )
+( cd resourcepack && zip -x "*.xcf" -r ../$DEST . )
 if [ -d $RSOURCEPACKS/ ]; then
   cp -iv $DEST $RSOURCEPACKS
 fi 
